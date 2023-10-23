71eb610dfa86589f36e99857ebaeb38b73811477
jauhara-nk <jauharakitty@gmail.com>
2023-10-23 20:15:43 +0300

    progress

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'; // You can import any icons you need
+{/*import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
+const element = <FontAwesomeIcon icon={faEnvelope} />
+
+ReactDOM.render(element, document.body)*/}
 
 function App() {
   return (
@@ -21,7 +27,7 @@ function App() {
           Learn React
         </a>
   </header>*/}
-<>
+
 <table className="table-design">
     <tbody>
       <tr>
@@ -39,24 +45,49 @@ function App() {
       </tr>
     </tbody>
   </table>
-  <h4>Overview</h4>
+  <h4 className="heading-style">Overview</h4>
   <ul className="list-design">
     <li>2 beds</li>
     <li>1 private bath</li>
   </ul>
+<br></br>
+  <p>
+    Experience the best of [City/Location] at [Hostel Name]. 
+    Our central location provides easy access to [Landmarks, Attractions], while our budget-friendly accommodations ensure you get the most out of your travel budget. 
+    Choose from a range of room options, from cozy private spaces to sociable dormitories.
+     Join our vibrant community of fellow travelers, share stories, and make lifelong friendships in our welcoming common areas.
+      With a commitment to cleanliness, safety, and sustainability, [Hostel Name] offers a comfortable and eco-conscious stay. 
+      Book your adventure today and discover the perfect hub for your unforgettable journey in [City/Location].
+      </p>
   <div>
-    <p>
+    <br></br>
+    <p className="heading-style">
       <b>This place offers</b>
     </p>
-    <ul>
+    <ul className="list-design">
       <li>tv</li>
       <li>wifi</li>
       <li><FontAwesomeIcon icon="fa-solid fa-utensils" />kitchen</li>
       <li>first aid</li>
       <li>transportation</li>
+      
     </ul>
-  </div>
-  </>
+    </div>
+    <br/>
+
+    <hr></hr>
+<br/>
+
+<h5><b>
+
+ Reviews</b></h5>
+
+
+   </div>
+   );
+  }
+  
+  export default App;
 
 
   

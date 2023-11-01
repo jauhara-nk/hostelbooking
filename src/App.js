555203c941b10739d40b5c5326f3c4df5ada20e5
jauhara-nk <jauharakitty@gmail.com>
2023-11-01 11:37:29 +0300

    icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
-import {BsWifi} from 'react-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {PiForkKnifeFill} from 'react-icons/pi';
+import {HiWifi} from 'react-icons/hi';
+import {PiTelevisionFill} from 'react-icons/pi';
+import {FaBus} from 'react-icons/fa';
+import {PiFirstAidLight} from 'react-icons/pi';
+import {CiHome} from 'react-icons/ci';
+
+{/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'; // You can import any icons you need
 import { IconBase } from 'react-icons/lib/esm';
-{/*import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
@@ -38,31 +44,32 @@ function App() {
   </table>
   <h4 className="heading-style">Overview</h4>
   <ul className="list-design">
-    <li>2 beds</li>
+    <li><CiHome />2 beds</li>
     <li>1 private bath</li>
   </ul>
 <br></br>
   <p>
     Experience the best of [City/Location] at [Hostel Name]. 
     Our central location provides easy access to [Landmarks, Attractions], while our budget-friendly accommodations ensure you get the most out of your travel budget. 
-    Choose from a range of room options, from cozy private spaces to sociable dormitories.
-     Join our vibrant community of fellow travelers, share stories, and make lifelong friendships in our welcoming common areas.
-      With a commitment to cleanliness, safety, and sustainability, [Hostel Name] offers a comfortable and eco-conscious stay. 
-      Book your adventure today and discover the perfect hub for your unforgettable journey in [City/Location].
+    Choose from a range of room options, from cozy private spaces to sociable dormitories.Join our vibrant community of fellow travelers, share stories, and make lifelong friendships in our welcoming common areas.
+    With a commitment to cleanliness, safety, and sustainability, [Hostel Name] offers a comfortable and eco-conscious stay. 
+    Book your adventure today and discover the perfect hub for your unforgettable journey in [City/Location].
       </p>
   <div>
     <br></br>
     <p className="heading-style">
       <b>This place offers</b>
     </p>
-    <ul className="column">
-      <li>tv</li>
-      <li>wifi</li>
-      <li><BsWifi />kitchen</li>
-      <li>first aid</li>
-      <li>transportation</li>
+    <ul className="list-design">
+      <li> <PiTelevisionFill/>tv</li>
+      <li> <HiWifi /> WiFi</li>
+      <li> <PiForkKnifeFill />kitchen</li>
+      <li><PiFirstAidLight />first aid</li>
+      <li><FaBus />transportation</li>
       
     </ul>
+
+    <button type= "button" >show all amenities</button>
     </div>
     <br/>
 
@@ -77,7 +84,7 @@ function App() {
    </div>
    );
   }
-  export IconBase;
+  
   
   export default App;
 
